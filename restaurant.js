6b74d2757b7eb97d6ce3a626e0456e3961b16b9b
ALIENWARE <tumwekwaseamiim@gmail.com>
2026-08-02 09:48:04 +0300

    Remove conflicting menu card animation

--- restaurant.js
+++ restaurant.js
@@ -89,58 +89,6 @@ window.addEventListener('scroll', ()=>{
 
 
 
-// =========================================
-// MENU CARD ANIMATION
-// =========================================
-
-
-const cards = document.querySelectorAll('.menu-item');
-
-
-const observer = new IntersectionObserver(
-
-(entries)=>{
-
-
-    entries.forEach(entry=>{
-
-
-        if(entry.isIntersecting){
-
-
-            entry.target.classList.add("show");
-
-
-            observer.unobserve(entry.target);
-
-
-        }
-
-
-    });
-
-
-},
-
-
-{
-
-threshold:0.15
-
-}
-
-);
-
-
-
-cards.forEach(card=>{
-
-    observer.observe(card);
-
-});
-
-
-
 
 
 // =========================================
